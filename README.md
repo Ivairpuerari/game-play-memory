1aa961b4bdfb27f47f2de7ba7e0ebaf292942de5
Ivair Puerari <ivaair@hotmail.com>
2022-09-25 23:47:14 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# game-play-memory
-Este é um repositório para a execução da Sprint da matéria de Gestão de Projetos de Software
+# gameplay
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Game Play',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+      ),
+      home: const MyHomePage(title: 'Jogo da Memória - Gestão de Projetos de Software'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  Widget _buildGrid() => Column(
+    children: [
+      const Text("RAPIDAMENTE MEMORIZE AS PALAVRAS ABAIXO!", style: TextStyle(fontSize: 20, color: Colors.grey, fontWeight: FontWeight.bold)),
+      const SizedBox(height: 20),
+      GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      scrollDirection: Axis.vertical,
+      crossAxisSpacing: 4,
+      shrinkWrap: true,
+      children: _buildGridTileList(10)),
+      const SizedBox(height: 100),
+      const Text("O JOGO EM BREVE IRA COMEÇAR...", style: TextStyle(fontSize: 20, color: Colors.grey, fontWeight: FontWeight.bold)),
+
+    ],
+  );
+
+  List<String> words = ["Gantt", "Scrum", "Sprint", "XP", "Kanban", "Agile", "PMBOK", "Backlog", "TAP", "EAP"];
+
+
+  List<Container> _buildGridTileList(int count) => List.generate(
+      count, (i) => Container(color: Colors.blueGrey,alignment: Alignment.center,  child: Text(words[i], style: const TextStyle(fontSize: 20, color: Colors.white))));
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title, style: const TextStyle(fontSize: 20, color: Colors.grey, fontWeight: FontWeight.bold)),
+        backgroundColor:  Colors.black,
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _counter == 0 ? Text(
+              "Clique no play e comece a jogar!",
+              style: TextStyle(fontSize: 40, color: Colors.grey),
+            ) : Container(),
+            _counter > 0 ? _buildGrid() : Container(),
+             const SizedBox(height: 30),
+            _counter == 0 ? FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Play',
+              child: const Icon(Icons.play_arrow),
+            ) : Container(),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
